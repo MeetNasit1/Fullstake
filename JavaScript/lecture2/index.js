e4a7f96aa6e37abbcfc5d2762ac11574ed8ea89b
MewtNasit1 <meetnasit918@gmail.com>
2024-08-29 18:11:24 +0530

    29-8-24

---
+++ JavaScript/lecture2/index.js
@@ -0,0 +1,32 @@
+// console.log('hello meet ')
+// console.log(20+60)
+// console.log(20+100)
+// console.log(20+780)
+// console.log(20+90)
+// console.log(20+'90'+30)
+// console.log(20+'90'+30-20)
+// console.log(20+'90'+30*20)
+
+
+
+// document.getElementById("box").innerHTML="this is javascript"
+document.getElementById("box").style.color="red"
+
+
+// javascript datatype
+
+// 1.primitive datatype
+
+
+// Number
+// String
+// Boolean
+// BigInt
+// undefined
+// null
+// Symbol
+
+// 2. non-primitive datatype
+
+// array
+// Object
