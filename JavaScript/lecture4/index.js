6bea36eceb292db6990660eed8bf06fde67f78d5
MewtNasit1 <meetnasit918@gmail.com>
2024-09-02 17:55:24 +0530

    2-9-24

---
+++ JavaScript/lecture4/index.js
@@ -0,0 +1,70 @@
+document.getElementById("demo") = "hello Meet"
+
+// javascript case
+
+// snake case
+
+// {
+//     let number_of =40;
+// console.log(number_of);
+// }
+
+// kebabe case
+
+// {
+//     let items-2 = 50;
+//     console.log(items-2);
+// }
+
+// camel case
+
+// {
+//     let myname = "hello"
+//     console.log(myname);
+// }
+
+
+// pascalcase
+
+// {
+//     let mynameis = "meet"
+//     console.log(mynameis);  
+// }
+
+// javascript template-literals
+
+// {
+//     let str1 = 'this is a string java !'
+
+//     let str2 =  "this is a string java ! "
+
+//     let str3 = `this is a string java !`
+
+//     console.log(str1);
+//     console.log(str2);
+//     console.log(str3);
+
+//     console.log(str1.length);
+//     console.log(str2.length);
+//     console.log(str2.length);
+
+// }
+
+
+{
+    // let str1='this is 
+// "string" javascript'
+//  let str2="this is
+//   string javascript"
+//  let str3=`this is  
+//  'string' javascript`
+
+//  console.log(str1);
+//  console.log(str2);
+//  console.log(str3);
+}
+
+{
+    
+}
+
