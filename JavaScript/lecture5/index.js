2191f79b75d3943557454c3ff7d88adc71df178f
MewtNasit1 <meetnasit918@gmail.com>
2024-09-06 16:10:52 +0530

    6-9-24

---
+++ JavaScript/lecture5/index.js
@@ -0,0 +1,143 @@
+
+// javascript operator
+
+// Arithmetic operator
+// Assigrment operator
+// Comparision operator
+// Conditional operator
+// logical operator
+// Unary operator
+// Binary operator
+// ternary operator
+// String operator
+
+
+// Arithmetic operator
+{
+    let x=5
+    let y=10 
+    console.log(x+z);
+    console.log(x-z);
+    console.log(x*z);
+    console.log(x/z);
+    console.log(x**z);
+    console.log(x%z);
+    
+}
+
+
+// Assigrment operator
+{
+    let a=20
+    let b=15
+    let c
+    console.log(c=a+=b);
+    console.log(a);
+
+    console.log(c=a-=b);
+    console.log(a);  
+    
+    console.log(c=a*=b);
+    console.log(a);
+
+    console.log(c=a/=b);
+    console.log(a);
+
+    console.log(c=a**=b);
+    console.log(a);
+
+    console.log(c=a%=b);
+    console.log(a);
+    
+}
+
+// Comparision operator
+{
+
+    {
+        let a = 10
+        let b = 10 
+    
+        console.log(a == a);
+    
+        let c = 'xyz'
+        let d = 'abc'
+    
+        console.log(c == d);
+    
+        let x = 'xyz'
+        let y = 'xyz'
+    
+        console.log(x == y);
+        
+    }
+    
+    {
+        let a = 10
+        let b = 10 
+    
+        console.log(a != a);
+    
+        let c = 'xyz'
+        let d = 'abc'
+    
+        console.log(c != d);
+    
+        let x = 'xyz'
+        let y = 'xyz'
+    
+        console.log(x != y);
+        
+    }
+    
+    {
+        let a = 10
+        let b = 10 
+    
+        console.log(a === a);
+    
+        let c = 'xyz'
+        let d = 'abc'
+    
+        console.log(c === d);
+    
+        let x = 'xyz'
+        let y = 'xyz'
+    
+        console.log(x === y);
+        
+    }
+    
+    {
+        let a = 10
+        let b = 10 
+    
+        console.log(a !== a);
+    
+        let c = 'xyz'
+        let d = 'abc'
+    
+        console.log(c !== d);
+    
+        let x = 'xyz'
+        let y = 'xyz'
+    
+        console.log(x !== y);
+        
+    }
+    
+    
+    
+    {
+        let num1 = 18
+        let num2 = 20
+    
+        console.log(num1<num2);
+        console.log(num1>num2);
+    
+    
+        
+     }
+
+}
+
