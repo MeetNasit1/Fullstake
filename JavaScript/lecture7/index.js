76507866dff0617ba708c25c1e09a8c9871c5475
MewtNasit1 <meetnasit918@gmail.com>
2024-09-07 17:49:12 +0530

    7-9-24

---
+++ JavaScript/lecture7/index.js
@@ -0,0 +1,192 @@
+// Conditional statement  
+
+// if statement 
+// if else statement
+// else ...... if statement 
+// Ledder if ... else statement 
+// Nested if ...else if  Statement
+// Switch case statement 
+
+// if statement 
+
+// if(codition){
+//     // statement 
+// }
+
+// {
+    // let age = 17;
+    // if(age < 18 ){
+    //     console.log("youre are not eligible for voting!")
+    // }
+// }
+
+// if else statement
+
+// if(codition){
+//     // statement 
+// }
+// else{
+//     statement 
+// }
+
+// {
+//     let age = 17;
+//     if(age < 18 ){
+//         console.log("youre are not eligible for voting!")
+//     }
+//     else{
+//         console.log("youre eligible for voting!");
+
+//     }
+// }
+
+
+// else ...... if statement - 
+
+
+// {
+//         let age = 17;
+//         let card = true;
+
+//         if(age < 18 ){
+//             console.log("youre are not eligible for voting!")
+//         }
+//         else if(card == true){
+//             console.log("youre are eligible for voting!")
+//         }
+//         else{
+//             console.log("youre eligible for voting!");
+
+//         }
+//  }
+
+
+// Nested if ...else if  Statement
+
+// {
+//             let age = 17;
+//             let card = false;
+
+//             if(age >= 18){
+//                 if(card == true){
+//                 console.log("youre are eligible for voting!")
+//                 }
+//             }
+//             else{
+//                 console.log("youre eligible for voting!");
+
+//             }
+//  }
+
+
+// switch case statement 
+
+// {
+//     let subject = "english";
+
+//     switch (subject) {
+//         case 'chemistry':
+//             console.log("chemistry book avaliable!")
+//             break;
+//         case 'english':
+//             console.log("english book avaliable!")
+//             break;
+
+//         case 'maths':
+//             console.log("maths book avaliable!")
+//             break;
+
+//         case 'computer':
+//             console.log("computer book avaliable!")
+//             break;
+
+//         case 'PT':
+//             console.log("PT book avaliable!")
+//             break;
+
+//         case 'Drawing':
+//             console.log("Drawing book avaliable!")
+//             break;
+
+//         case 'Science':
+//             console.log("Science book avaliable!")
+//             break;
+
+
+//     }
+// }
+
+
+// Javascript Loops  --
+
+// while loop
+// do-while loop
+// for loop
+// for ..each loop
+// for ..of loop
+// for ..in loop
+// nested loop
+// loop with break statement 
+// loop with continue statement
+
+
+//while loop
+
+// while(condition){
+//     statements,
+//     afterthought,
+// }
+
+// {
+//     let X = 5;
+
+//     while(X <=10){{
+//         console.log(X," while loop");
+//         X++;
+//     }}
+// }
+
+// do-while loop
+
+// do{
+//     statement 
+// }while(condition)
+
+// {
+//     do{
+//         console.log(X," while loop");
+//         X++;
+//     }while(X > 15)
+// }
+
+
+// for loop
+
+// for(intialization; condition; afterthough){
+//     statement 
+// }
+
+// {
+//     let i = 25
+//     for(i; i< 50; i++){
+//         console.log("for loop", i)
+//     }
+// }
+
+
+// for ..each loop
+
+// Array.forEach(function(currentvalue, index , arrr))
+
+
+
+// for ..of loop
+
+// for(variable of iterable)
+//     statement 
+
+
+// for ..in loop
+
+// for(variable of object)
+//     statement
