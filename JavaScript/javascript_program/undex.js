5f8bea760710a108e5491a96feef5f7999c44373
MewtNasit1 <meetnasit918@gmail.com>
2024-09-23 17:54:35 +0530

    23-9-24

---
+++ JavaScript/javascript_program/undex.js
@@ -0,0 +1,69 @@
+// 1) JavaScript Program To Print Hello World
+{
+    let A = 'hello world';
+    console.log(A);
+    
+}
+
+// 2) JavaScript Program to Add Two Numbers
+{
+    let a=5;
+    let b=10;
+    let c=a*b;
+
+    console.log(c);
+
+}
+
+// 3) JavaScript Program to Find the Square Root
+
+{
+    let a=Math.sqrt(50);
+    console.log(a);  
+}
+
+// 4) JavaScript Program to Calculate the Area of a Triangle
+{
+    let height=10;
+    let width=50;
+    let Triangle=height*width/2;
+
+    console.log(Triangle);
+    
+}
+
+// 5) JavaScript Program to Swap Two Variables
+{
+    let x=50;
+    let y=100;
+    let z=40;
+
+    z=x;
+    x=y;
+    y=z
+
+    console.log(x);
+    console.log(y);
+}
+
+// 6) JavaScript Program to Solve Quadratic Equation
+{
+    
+}
+
+
+
+// 7) JavaScript Program to Convert Kilometres to Miles
+{
+    let Kilometres=5;
+    let Miles=Kilometres*0.62
+
+    console.log(Miles);
+}
+
+
+
+
+
+
+
