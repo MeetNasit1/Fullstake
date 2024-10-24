70708d63a7f494ed0f3fd85f0e00ed7bf9c7a973
meetnasit <meetnasit918@gmail.com>
2024-10-24 16:04:41 +0530

    24-10-24

---
+++ JQuery/lecture4 program/index.js
@@ -0,0 +1,20 @@
+// Exercise - 1 - Check if jQuery is loaded
+
+// if(jQuery === "undefined")
+//     {
+//     console.log("jQuery not add");
+//     }
+// else{
+//     console.log("jQuery add");
+    
+// }
+
+// Exercise - 2 - If Page is too long scroll to the Top
+
+// $(function(){
+//     $('#btn').click(function(){
+//         $('html,body').animate({
+//             scrollTop: 0
+//         },2000)
+//     })
+// })
