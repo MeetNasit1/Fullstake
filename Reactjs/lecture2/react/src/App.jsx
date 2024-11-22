3fb6a04bedf929d4832d48f03e563bd00185aaae
meetnasit <meetnasit918@gmail.com>
2024-11-22 17:46:32 +0530

    22-11-24

---
+++ Reactjs/lecture2/react/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Button from './component/Button'
+import { Card } from './component/Card'
+
+function App() {
+
+  return (
+    <>
+      <Button/>
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ Reactjs/lecture2/react/src/component/Card.jsx
@@ -0,0 +1,15 @@
+export const Card = () => {
+  return (
+    <>
+      <div className="card">
+        <img src="img_avatar.png" alt="Avatar" />
+        <div className="container">
+          <h4>
+            <b>John Doe</b>
+          </h4>
+          <p>Architect & Engineer</p>
+        </div>
+      </div>
+    </>
+  );
+};
