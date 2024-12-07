c5ff971dda8fe6cff7a6ae7eed0c31219906750b
meetnasit <meetnasit918@gmail.com>
2024-12-07 17:52:56 +0530

    7-12-24

--- Reactjs/lecture8bootstrap/bootstrap/src/App.js
+++ Reactjs/lecture8bootstrap/bootstrap/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 import Bootstrap from './component/Bootstrap';
+import StaticExample from './component/Modal';
+import Tab from './component/Tabs';
 
 
 function App() {
   return (
     <>
     <Bootstrap/>
+    <StaticExample/>
+    <Tab/>
     </>
   );
 }
 
 export default App;
+
+
